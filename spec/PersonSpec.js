ac97147f5e11b2932322694cdf095afb9e007de6
Jordan Morano <jordan.morano@gmail.com>
2014-02-19 16:15:49 -0500

    Person must have a first name and a last name

--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -1,3 +1,26 @@
 describe("Person", function () {
+  var person;
 
+  it("should be initialized with first and last names", function() {
+    person = new Person("Jordan", "Morano");
+
+    expect(person.firstName).toEqual("Jordan");
+    expect(person.lastName).toEqual("Morano");
+    expect(function(){
+      bad = new Person("Jordan");
+    }).toThrowError("A valid person must have a first and last name.")
+  });
+
+  describe("Check optional properties.", function(){
+    it("has optional properties with default values",function(){
+      person = new Person("Yiyang", "Song");
+      expect(person.age).toBeLessThan(46);
+      expect(person.age).toBeGreaterThan(24);
+      expect(person.weight).toBeLessThan(100);
+      expect(person.weight).toBeGreaterThan(60);
+      expect(person.height).toBeLessThan(200);
+      expect(person.height).toBeGreaterThan(150);
+      expect(person.friends instanceof Array).toBeTruthy();
+    });
+  });
 });
--- src/Person.js
+++ src/Person.js
@@ -1,3 +1,17 @@
-var Person = function ( ) {
-
+var Person = function (firstName, lastName, options) {
+  if(lastName === undefined){
+    throw new Error("A valid person must have a first and last name.");
+  }
+  this.firstName = firstName;
+  this.lastName = lastName;
+  // set default attributes
+  this.age = Math.floor(Math.random() * (46 - 24 + 1) + 24);
+  this.weight = Math.floor(Math.random() * (100 - 60 + 1) + 60);
+  this.height = Math.floor(Math.random() * (200 - 150 + 1) + 150);
+  this.friends = []
+  // if ( options !== undefined ) {
+  //   for ( prop in options ) {
+  //     options.prop
+  //   }
+  // }
 };
