b0f6752c35f6d3c55a302eee566f4c4768ccebb2
Mike Talley <miketalley@hotmail.com>
2014-05-28 18:09:32 -0400

    Finished test on basic info and set up functions

--- spec/javascripts/PersonSpec.js
+++ spec/javascripts/PersonSpec.js
@@ -1,7 +1,33 @@
 describe('Person', function() {
   beforeEach(function() {
-    this.person = new Person();
+    this.person = new Person('Mike', 'Talley');
+  });
+
+  it('initializes name, age, weight, height, and friends', function() {
+    expect(this.person.firstName).toBe('Mike');
+    expect(this.person.lastName).toBe('Talley');
+    expect(this.person.age).toBe(30);
+    expect(this.person.weight).toBe(80);
+    expect(this.person.height).toBeLessThan(200);
+    expect(this.person.height).toBeGreaterThan(150);
+    expect(this.person.friends.length).toBe(0);
+  });
 
 });
 
+describe('Person', function() {
+  beforeEach(function() {
+    this.person = new Person('Mike', 'Talley', 34, 86, 182, ['Ethan', 'Lisa', 'George', 'Amanda']);
+  });
+
+  it('accepts values as parameters', function() {
+    expect(this.person.firstName).toBe('Mike');
+    expect(this.person.lastName).toBe('Talley');
+    expect(this.person.age).toBe(34);
+    expect(this.person.weight).toBe(86);
+    expect(this.person.height).toBe(182);
+    expect(this.person.friends.length).toBe(4);
+  });
 });
+
+
