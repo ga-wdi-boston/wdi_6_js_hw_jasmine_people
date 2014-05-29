dc28b762c7b4a185228afb47403883e0a14d1f20
Kathleen Votolato <kvotolato3@gmail.com>
2014-05-28 22:26:38 -0400

    Refactor Jasmine tests on Person properties

--- spec/javascripts/PersonSpec.js
+++ spec/javascripts/PersonSpec.js
@@ -1,5 +1,9 @@
 describe("Person", function(){ // Describing the class we want to look at
   describe("initializer", function(){ // Each area of functionality we would like to test.
+    beforeEach(function() {
+    this.defaultPerson = new Person();
+    });
+
     it("accepts a first and last name", function(){
       var person = new Person("Jon", "Doe");
 
@@ -7,36 +11,32 @@ describe("Person", function(){ // Describing the class we want to look at
       expect(person.lastName).toEqual("Doe");
     });
     it("has a default age", function(){
-      var person = new Person("Jon", "Doe");
-      expect(person.age).toEqual(30);
+      expect(this.defaultPerson.age).toEqual(30);
     });
     it("accepts an age", function(){
       var person = new Person("Jon", "Doe", 33);
       expect(person.age).toEqual(33);
     });
     it("has a default weight", function(){
-      var person = new Person();
-      expect(person.weight).toEqual(80);
+      expect(this.defaultPerson.weight).toEqual(80);
     });
     it("accepts a weight", function(){
       var person = new Person("Jon", "Doe", 33, 64);
       expect(person.weight).toEqual(64);
     });
     it("has a default height", function(){
-      var person = new Person();
       // The default Height is a randomly generated number between
       // 150 and 200.  This test ensures the number falls within that range.
-      expect(person.height).not.toBeLessThan(150);
-      expect(person.height).not.toBeGreaterThan(200);
-      expect(person.height).not.toBeUndefined();
+      expect(this.defaultPerson.height).not.toBeLessThan(150);
+      expect(this.defaultPerson.height).not.toBeGreaterThan(200);
+      expect(this.defaultPerson.height).not.toBeUndefined();
     });
     it("accepts a height", function(){
       var person = new Person("Jon", "Doe", 33, 64, 155);
       expect(person.height).toEqual(155);
     });
     it("has a default empty array of friends", function(){
-      var person = new Person();
-      expect(person.friends).toEqual([]);
+      expect(this.defaultPerson.friends).toEqual([]);
     });
     it("accepts an array of friends(person objects)", function(){
       var friend1 = new Person();
