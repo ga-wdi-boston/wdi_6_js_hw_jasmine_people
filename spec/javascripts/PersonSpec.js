96538fb89802f2064566c75c043fd0e79c76af78
George Straehle <gstraehle@gmail.com>
2014-05-28 23:19:24 -0400

    failing person properties test

--- spec/javascripts/PersonSpec.js
+++ spec/javascripts/PersonSpec.js
@@ -7,6 +7,12 @@ describe("Person", function(){
     it("initializes firstName", function(){
 
     expect(this.person.firstName).toEqual("George");
+    expect(this.person.lastName).toEqual("Straehle");
+    expect(this.person.age).toEqual(30);
+    expect(this.person.weight).toEqual(80);
+    expect(this.person.height).toBe(100);
+    expect(this.person.friends.length).toEqual(0);
   });
 });
 });
+
