ad18d5e13c096b8dc6cb7d9404f90cac0b0ed984
Amanda McDermott <amandapmcdermott@gmail.com>
2014-05-28 22:40:40 -0400

    Specced optional properties

--- spec/javascripts/PersonSpec.js
+++ spec/javascripts/PersonSpec.js
@@ -7,4 +7,28 @@ describe("Person", function(){
       expect(person.LastName).toEqual("Loblaw");
     });
   });
+
+  beforeEach(function() {
+    this.person = new Person("Bob", "Loblaw");
+  });
+
+  describe("properties", function(){
+    it("include age, which defaults to 30", function(){
+      this.person.age = 30;
+
+      expect(this.person.age).toEqual(30);
+    });
+
+    it("include weight, which defaults to 80", function(){
+      this.person.weight = 80;
+
+      expect(this.person.weight).toEqual(80);
+    });
+
+    it("include friends array, which defaults to empty", function(){
+      this.person.friends = [];
+
+      expect(this.person.weight).toEqual(80);
+    });
+  });
 });
