92889212122e1a8c199d9037a05be6225fe425c9
Christine Lafontant <Christine@Christines-MacBook-Pro.local>
2014-05-29 07:41:58 -0400

    uppdated first spec

--- app/assets/javascripts/Person.js
+++ app/assets/javascripts/Person.js
@@ -1,20 +1,25 @@
-var Person = function (firstName, lastName, ageinYears, weightinKilograms, heightinCentimeters, friends) {
+var Person = function(firstName, lastName, ageinYears, weightinKilograms, heightinCentimeters, friends) {
 	this.firstName = firstName; 
 	this.lastName = lastName;
-	if (typeof ageinYears !== undefined) {
-		ageinYears = 30; 
+	if (typeof ageinYears === null ) {
+		this.ageinYears = 30; 
 	}
-	if (typeof weightinKilograms !== undefined) {
-		weightinKilograms = 80; 
+	if (typeof weightinKilograms === null ) {
+		this.weightinKilograms = 80; 
 	}
-	if (typeof heightinCentimeters !== undefined) {
-		heightinCentimeters = (Math.floor(Math.random() * 50) + 150); 
+	if (typeof heightinCentimeters === null ) {
+		this.heightinCentimeters = (Math.floor(Math.random() * 51) + 150); 
 	}
-	if (typeof friends !== undefined) {
-		friends = [ ]; 
-		// friends = ["Ironman", "Pinky", "The Brain", "Sheldon Cooper"][Math.floor(Math.random() * 4)]; 
+	if (typeof friends === null ) {
+		this.friends = [ ]; 
 	}
 }; 
+
+// Person.prototype = {
+// 	fullName: function() {
+// 		return this.firstName + ' ' +  this.lastName;
+// 	}
+// };
 
 
 
--- spec/javascripts/support/PersonSpec.js
+++ spec/javascripts/support/PersonSpec.js
@@ -1,41 +1,78 @@
 describe("Person", function() {
 	
 	describe("intializer", function() {
-		it("accepts a first name and a last name and optional arguments", function (){
+		it("accepts a first name and a last name", function() {
 			this.person = new Person("Chuck", "Norris");
 
 			expect(this.person.firstName).toEqual("Chuck");
 			expect(this.person.lastName).toEqual("Norris");
-			if (this.person.ageinYears !== undefined) {
-				expect(this.person.ageinYears).toEqual(30);
-			}
-			else {
-				expect(this.person.ageinYears).toEqual(this.person.ageinYears);
-			}
-			
-			if (this.person.weightinKilograms !== undefined) {
-				expect(this.person.weightinKilograms).toEqual(80);
-			}
-			else {
-				expect(this.person.weightinKilograms).toEqual(this.person.weightinKilograms);
-			}
+		});
 
-			if (this.person.heightinCentimeters !== undefined) {
-				expect(this.person.heightinCentimeters).toEqual((Math.floor(Math.random() * 50) + 150));
-			}
-			else {
-				expect(this.person.heightinCentimeters).toEqual(this.person.heightinCentimeters);
-			}
+		it("has optional properties with default values", function() {
+			this.person = new Person("Melissa", "McCarthy");
 
-			if (this.person.friends !== undefined) {
-				expect(this.person.friends).toEqual([ ]);
-			}
-			else {
-				expect(this.person.friends).toEqual(this.person.friends);
-			}
+			expect(this.person.ageinYears).toBeNull(30);
+			expect(this.person.weightinKilograms).toBeNull(80);
+			expect(this.person.heightinCentimeters).toBeGreaterThan(150);
+			expect(this.person.heightinCentimeters).toBeLessThan(200);
+			expect(this.person.friends).toBeNull([ ]);
+		});
+	}); 
 
-		}); 
-	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+	// 		expect(this.person.firstName).toEqual(this.person.firstName);
+	// 		expect(this.person.lastName).toEqual(this.person.lastName);
+	// 		if (this.person.ageinYears === undefined) {
+	// 			expect(this.person.ageinYears).toEqual(30);
+	// 		}
+	// 		else {
+	// 			expect(this.person.ageinYears).toEqual(this.person.ageinYears);
+	// 		}
+			
+	// 		if (this.person.weightinKilograms === undefined) {
+	// 			expect(this.person.weightinKilograms).toEqual(80);
+	// 		}
+	// 		else {
+	// 			expect(this.person.weightinKilograms).toEqual(this.person.weightinKilograms);
+	// 		}
+
+	// 		if (this.person.heightinCentimeters === undefined) {
+	// 			expect(this.person.heightinCentimeters).toEqual((Math.floor(Math.random() * 50) + 150));
+	// 		}
+	// 		else {
+	// 			expect(this.person.heightinCentimeters).toEqual(this.person.heightinCentimeters);
+	// 		}
+
+	// 		if (this.person.friends === undefined) {
+	// 			expect(this.person.friends).toEqual([ ]);
+	// 		}
+	// 		else {
+	// 			expect(this.person.friends).toEqual(this.person.friends);
+	// 		}
+
+	// 	}); 
+	// });
+
+	// describe("#fullName", function() {
+	// 	it ("returns the person's first name and last name", function() {
+	// 		this.person.fullName();
+
+	// 		expect(this.person.fullName).toEqual(this.person.firstName + " " + this.person.lastName);
+
+
+
+
 
 
