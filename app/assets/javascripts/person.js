d949f6a13728b542ce2f2268a4f30c6b1bbe9838
EricRho <eric.rho.87@gmail.com>
2014-07-17 09:10:12 -0400

    Forgot all my other commits... Sorry... Assignment finished, me thinks

---
+++ app/assets/javascripts/person.js
@@ -0,0 +1,39 @@
+var Person = function(firstName, lastName, age, weight, height, friends) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age || 30;
+  this.weight = weight || 80;
+  this.height = height || Math.floor(Math.random() * (200 - 150 + 1) + 150);
+  this.friends = friends || [];
+};
+
+Person.prototype.fullName = function(firstName, lastName) {
+  return this.firstName + " " + this.lastName;
+};
+
+Person.prototype.weightInStone = function(weight) {
+  return this.weight * 0.157473;
+};
+
+Person.prototype.addFriend = function(friend) {
+  this.friends.push(friend);
+  return this.friends;
+};
+
+Person.prototype.removeFriend = function(friend) {
+  for(var i = 0; i < this.friends.length; i++) {
+    if(this.friends[i].firstName === friend)
+      this.friends.splice(this.friends[i]);
+    return this.friends;
+  }
+};
+
+Person.prototype.greetPeople = function(people, greeting) {
+  this.people = people || this.friends;
+  this.greeting = greeting || 'Hi';
+  var shoutout = this.greeting;
+
+  this.people.forEach(function(person) {
+    console.log(shoutout + " " + person.fullName());
+  });
+};
